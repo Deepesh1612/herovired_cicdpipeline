4741abfac62737d7a636f80ec7cc49d6e81c72e5
Deepesh1612 <deepeshsahu1612@gmail.com>
2025-08-23 16:58:45 +0000

    something wrong

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <h1>
-<<<<<<< HEAD
-hello deepesh \n
-from Dev branch to prod branch push \n
-git checkout and copy \n
-index file
-
-=======
-Hello Deepesh ||
-From Dev branch to prod branch push ||
 Git checkout and copy ||
 Index file ||
 3rd attempt
->>>>>>> 5b2d02a2085655bdd82b6e842d23ce0397adeb72
 </h1>
